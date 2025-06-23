bf6948ad5dab88c781b35503b65a839ba71b3714
Marcus Santos <marcuscruzsantos27@gmail.com>
2025-06-23 15:35:57 -0300

    feat: adding Resender api system for email contact

--- apps/web/src/app/components/ContactForm.tsx
+++ apps/web/src/app/components/ContactForm.tsx
@@ -9,16 +9,59 @@ import {
   DialogContent,
   DialogDescription,
   DialogTitle,
-  DialogTrigger,
 } from '@radix-ui/react-dialog';
+import { useState } from 'react';
 
 function ContactForm({
   open,
   onOpenChange,
+  api,
 }: {
   open: boolean;
   onOpenChange: (open: boolean) => void;
+  api: string;
 }) {
+  const [name, setName] = useState('');
+  const [subject, setSubject] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      const response = await fetch(api, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name,
+          emailSubject: subject,
+          sender: email,
+          content: message,
+        }),
+      });
+
+      if (response.ok) {
+        setLoading(false);
+        setName('');
+        setSubject('');
+        setEmail('');
+        setMessage('');
+        onOpenChange(false);
+      } else {
+        setLoading(false);
+        throw new Error('Failed to send message');
+      }
+    } catch (error) {
+      setLoading(false);
+      console.error('Error sending message:', error);
+    }
+  };
+
   const pathName = usePathname();
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
@@ -40,25 +83,37 @@ function ContactForm({
               <input
                 className="w-full p-2 border border-neutral-400 rounded"
                 placeholder="Name"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
               />
               <input
                 className="w-full p-2 border border-neutral-400 rounded"
                 placeholder="Subject"
+                type="text"
+                value={subject}
+                onChange={(e) => setSubject(e.target.value)}
               />
               <input
                 className="w-full p-2 border border-neutral-400 rounded"
                 placeholder="Email"
                 type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
               <textarea
                 className="w-full size-40 resize-none p-2 border border-neutral-400 rounded"
                 placeholder="Message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
               />
               <Button
+                onClick={handleSubmit}
+                disabled={loading || !name || !subject || !email || !message}
                 type="submit"
                 className={`${pathName.includes('marcus') ? 'bg-[#22c55e]/70 hover:bg-[#22c55e]' : 'bg-violet-500/50 hover:bg-violet-500/80'} mt-4 cursor-pointer`}
               >
-                Send
+                {loading ? 'Sending...' : 'Send'}
               </Button>
             </form>
           </div>
