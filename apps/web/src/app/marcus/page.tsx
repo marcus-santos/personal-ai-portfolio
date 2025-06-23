9ad9342c241efe14c0642509044bd114dadfbc95
Marcus Santos <marcuscruzsantos27@gmail.com>
2025-06-23 12:25:46 -0300

    feat: landing page finished, animations included on the other pages.

--- apps/web/src/app/marcus/page.tsx
+++ apps/web/src/app/marcus/page.tsx
@@ -1,58 +1,66 @@
 'use client';
 
-import { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import { marcus } from '../types/header-user';
 import Footer from '../components/Footer';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import { motion } from 'framer-motion';
 
 function page() {
-  const [showContent, setShowContent] = useState(false);
-  const [showH1, setShowH1] = useState(false);
-  const [showP, setShowP] = useState(false);
-  const [showImg, setShowImg] = useState(false);
+  const container = {
+    hidden: {},
+    show: {
+      transition: {
+        staggerChildren: 0.5,
+      },
+    },
+  };
 
-  useEffect(() => {
-    // Sequência de animações: h1 -> p -> imagem/botão
-    const timers: NodeJS.Timeout[] = [];
-    timers.push(setTimeout(() => setShowContent(true), 200));
-    timers.push(setTimeout(() => setShowH1(true), 400));
-    timers.push(setTimeout(() => setShowP(true), 800));
-    timers.push(setTimeout(() => setShowImg(true), 1000));
-    return () => timers.forEach(clearTimeout);
-  }, []);
+  const item = {
+    hidden: { opacity: 0, y: 0 },
+    show: { opacity: 1, y: 0 },
+  };
 
   return (
     <div className="flex flex-col h-screen">
       <Header {...marcus} />
-      <div className={`flex w-2/3 mx-auto h-full my-40`}>
-        <div className="flex flex-col m-auto">
-          <h1
-            className={`text-4xl font-bold transition-all duration-700
-              ${showH1 ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-10'}
-            `}
-            style={{ transitionProperty: 'opacity, transform' }}
+      <main className={`flex w-2/3 mx-auto h-full my-40`}>
+        <motion.div
+          variants={container}
+          initial="hidden"
+          animate="show"
+          className="flex flex-col gap-4 my-auto w-full lg:w-1/2"
+        >
+          <motion.h1
+            variants={item}
+            className="text-4xl font-bold flex items-center gap-2"
           >
-            Hi, I&apos;m Marcus 👋
-          </h1>
-          <p
-            className={`text-white/70 mt-2 max-w-xl transition-all duration-700
-              ${showP ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-10'}
-            `}
-            style={{ transitionProperty: 'opacity, transform' }}
-          >
+            Hi, I&apos;m Marcus{' '}
+            <motion.span
+              className="inline origin-[70%_70%] mr-15 lg:mr-0"
+              animate={{ rotate: [0, 20, -10, 20, 0] }}
+              transition={{
+                repeat: Infinity,
+                duration: 2,
+                repeatDelay: 3,
+                delay: 0.5,
+              }}
+            >
+              👋
+            </motion.span>
+          </motion.h1>
+
+          <motion.p variants={item} className="text-white/70">
             Full-stack developer focused on building scalable and accessible
             digital experiences.
-          </p>
+            <br /> I&apos;m passionate about what I do, and nothing beats
+            solving a problem while sipping a great coffee. Feel free to explore
+            my work or talk to my assistant if you want to know more about me.
+          </motion.p>
 
-          <div
-            className={`mt-6 flex gap-4 items-center transition-all duration-700
-              ${showImg ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-10'}
-            `}
-            style={{ transitionProperty: 'opacity, transform' }}
-          >
+          <motion.div variants={item} className="flex gap-4 mt-6 items-center">
             <Button asChild className="bg-[#22c55e]/60 hover:bg-[#22c55e]/80">
               <Link href={marcus.chatRef}>Ask MarcusBot</Link>
             </Button>
@@ -62,21 +70,22 @@ function page() {
             >
               See my work
             </Link>
-          </div>
-        </div>
-        <div
-          className={`max-w-[450px] w-full h-full m-auto relative min-h-[450px] hidden lg:block transition-all duration-700
-            ${showImg ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-10'}
-          `}
-          style={{ transitionProperty: 'opacity, transform' }}
+          </motion.div>
+        </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 1.2, duration: 0.6 }}
+          className="w-1/2 h-full max-h-[400px] hidden lg:block my-auto"
         >
           <DotLottieReact
             src="https://lottie.host/a3d417a4-df86-4dc3-8f0a-831687dff82a/jLPCKfdZo1.lottie"
             loop
             autoplay
           />
-        </div>
-      </div>
+        </motion.div>
+      </main>
       <Footer />
     </div>
   );
